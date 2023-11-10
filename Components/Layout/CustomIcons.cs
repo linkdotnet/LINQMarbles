6e190f9c1a7267e8f68bccab9194160016b9f0fa
Steven Giesel <stgiesel35@gmail.com>
2023-11-10 13:08:32 +0100

    Footer and links

---
+++ Components/Layout/CustomIcons.cs
@@ -0,0 +1,6 @@
+namespace LinqMarbles.Components.Layout;
+
+public static class CustomIcons
+{
+    public const string Triforce = "<svg viewBox=\"0 0 512 512\" xmlns=\"http://www.w3.org/2000/svg\" fill=\"#000000\"><g stroke-width=\"0\"></g><g id=\"SVGRepo_tracerCarrier\" stroke-linecap=\"round\" stroke-linejoin=\"round\"></g><g id=\"SVGRepo_iconCarrier\"><path fill=\"#000000\" d=\"M256 59.72L142.687 256h226.625L256 59.72zM369.313 256L256 452.28h226.625L369.312 256zM256 452.28L142.687 256 29.376 452.28H256z\"></path></g></svg>";
+}
