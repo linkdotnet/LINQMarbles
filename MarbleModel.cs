05a3f3a81c2d0021d9fee4674fdd9d4547e69e4a
Steven Giesel <stgiesel35@gmail.com>
2023-11-10 15:22:52 +0100

    More operators

--- MarbleModel.cs
+++ MarbleModel.cs
@@ -4,12 +4,17 @@ namespace LinqMarbles;
 
 public record MarbleModel(int Number)
 {
-    public Color Color { get; } = GetRandomColor();
+    public Color Color { get; init; } = GetRandomColor();
 
     private static Color GetRandomColor()
     {
         Span<Color> colors = [Color.Primary, Color.Secondary, Color.Info, Color.Success, Color.Warning, Color.Error, Color.Dark, Color.Surface];
         var randomIndex = Random.Shared.Next(colors.Length);
         return colors[randomIndex];
+    }
+
+    public virtual bool Equals(MarbleModel? other)
+    {
+        return Number == other?.Number;
     }
 }
