94d857c70f168be8aa3ac61d0b2875bb66136885
Steven Giesel <stgiesel35@gmail.com>
2023-11-13 17:23:29 +0100

    Use Blazor Web (closes #4)

---
+++ LinqMarbles.Client/Program.cs
@@ -0,0 +1,8 @@
+using LinqMarbles.Client.Components;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.Services.AddMudServices();
+
+await builder.Build().RunAsync();
---
+++ LinqMarbles/Program.cs
@@ -0,0 +1,33 @@
+using LinqMarbles.Client.Components;
+using MudBlazor.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+builder.Services.AddMudServices();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
+    .AddInteractiveWebAssemblyRenderMode();
+
+app.Run();
