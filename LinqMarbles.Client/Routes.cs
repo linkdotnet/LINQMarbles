036fa6259017feb554a7f10fd4a24f0ef1c97f53
Diego Faria <diego.rdfaria@gmail.com>
2023-11-17 21:30:36 -0500

    New class to manage routes

---
+++ LinqMarbles.Client/Routes.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Frozen;
+
+namespace LinqMarbles.Client
+{
+    public static class Routes
+    {
+        public static readonly FrozenDictionary<string, string[]> NavGroup = new Dictionary<string, string[]>
+        {
+            { "Aggregation", ["Aggregate", "Average", "Count", "Max", "MaxBy", "Min", "MinBy", "Sum"] },
+            { "Concatenation", [ "Concat" ] },
+            { "Element", [ "First", "FirstOrDefault", "Last", "LastOrDefault", "Single", "SingleOrDefault" ] },
+            { "Filtering", [ "Distinct", "Skip", "Take", "Where" ] },
+            { "Grouping", [ "GroupBy" ] },
+            { "Merging", [ "GroupJoin", "Join", "Zip" ] },
+            { "Order", [ "Order", "OrderDescending" ] },
+            { "Projection", [ "Select", "SelectMany" ] },
+            { "Quantification", [ "All", "Any", "Contains", "SequenceEqual" ] },
+            { "Sequence", [ "Chunk" ] },
+            { "Set", [ "Except", "Intersect", "Union" ] },
+        }.ToFrozenDictionary();
+        
+        public static string FormatLinkHref(string link) => 
+            "/" + string.Concat(link.Select((c, i) => i > 0 && char.IsUpper(c) ? "-" + c : c.ToString())).ToLower();
+    }
+}
